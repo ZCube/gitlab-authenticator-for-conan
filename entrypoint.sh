27565fa0cd4c5bf91d1857c7afadaa1ba0e83b6a
ZCube <zcube@zcube.kr>
2018-04-05 22:16:56 +0900

    debug..

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,5 @@ set -e -x
 chown conan -R /conan/.conan_server
 chown conan -R /conan
 chmod +r /conan/.conan_server/*.conf
-find /conan/.conan_server/
-ls -alR /conan/.conan_server/
 cat /conan/.conan_server/gitlabauth.conf
 su -c /usr/local/bin/conan_server conan
